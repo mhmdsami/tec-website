3c4bb14ce956d32f48608c638067357cb86faa48
Mohamed Sami <srmf2025@gmail.com>
2024-06-22 08:47:26 +0530

    feat: images

--- app/routes/_app._index.tsx
+++ app/routes/_app._index.tsx
@@ -6,6 +6,7 @@ import {
   json,
 } from "@remix-run/node";
 import { Link, useLoaderData, useOutletContext } from "@remix-run/react";
+import Autoplay from "embla-carousel-autoplay";
 import { BlogCard, EventCard } from "~/components/cards";
 import { Button } from "~/components/ui/button";
 import { Card, CardContent } from "~/components/ui/card";
@@ -16,9 +17,9 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "~/components/ui/carousel";
+import { AppOutletContext } from "~/routes/_app";
 import siteConfig from "~/site.config";
 import { db } from "~/utils/db.server";
-import { AppOutletContext } from "~/routes/_app";
 
 export const meta: MetaFunction = () => {
   return [
@@ -56,7 +57,7 @@ export default function Index() {
 
   return (
     <main className="mb-10 flex flex-col gap-20">
-      <div className="flex h-[85vh] flex-col-reverse items-center justify-center gap-20 sm:flex-row lg:gap-0">
+      <div className="flex h-[85vh] flex-col items-center justify-center gap-20 sm:flex-row lg:gap-0">
         <div className="sm:basis-1/2">
           <h1 className="text-4xl font-bold lg:text-7xl">
             Tirunelveli Economic Chamber
@@ -66,9 +67,43 @@ export default function Index() {
             economic interests of its members and the broader community.
           </p>
         </div>
-        <div className="sm:basis-1/2">
-          <img src="/tec.png" alt="TEC Logo" />
-        </div>
+        <Carousel
+          className="sm:basis-1/2"
+          plugins={[
+            Autoplay({
+              delay: 5000,
+              stopOnInteraction: true,
+              stopOnMouseEnter: true,
+            }),
+          ]}
+        >
+          <CarouselContent>
+            <CarouselItem className="flex aspect-video items-center justify-center p-6">
+              <img src="/tec.png" alt="TEC Logo" />
+            </CarouselItem>
+            {events.map(
+              ({ images }, idx) =>
+                images.length > 0 && (
+                  <CarouselItem key={idx}>
+                    <Card>
+                      <CardContent className="flex aspect-video flex-col items-center justify-center gap-2 p-6">
+                        <img
+                          src={images[0].url}
+                          alt={images[0].description || "Event image"}
+                          className="aspect-video object-contain"
+                        />
+                        <Button asChild className="self-end">
+                          <Link to="/events" prefetch="intent">
+                            View Event
+                          </Link>
+                        </Button>
+                      </CardContent>
+                    </Card>
+                  </CarouselItem>
+                ),
+            )}
+          </CarouselContent>
+        </Carousel>
       </div>
       <div className="flex flex-col gap-5 md:flex-row" id="about">
         <div className="flex basis-1/2 flex-col gap-2">
@@ -94,15 +129,22 @@ export default function Index() {
             provide social services
           </p>
         </div>
-        <Carousel className="basis-1/2">
+        <Carousel
+          className="basis-1/2"
+          plugins={[
+            Autoplay({
+              delay: 5000,
+            }),
+          ]}
+        >
           <CarouselContent>
-            {Array.from({ length: 5 }).map((_, index) => (
+            {Array.from({ length: 9 }).map((_, index) => (
               <CarouselItem key={index}>
-                <Card>
-                  <CardContent className="flex aspect-video items-center justify-center p-6">
-                    <span className="text-4xl font-semibold">{index + 1}</span>
-                  </CardContent>
-                </Card>
+                <img
+                  src={`/images/${index + 1}.jpeg`}
+                  alt="about us image"
+                  className="aspect-video object-contain"
+                />
               </CarouselItem>
             ))}
           </CarouselContent>
@@ -147,7 +189,7 @@ export default function Index() {
               <Link to="/dashboard" prefetch="intent">
                 Dashboard
               </Link>
-              ) : (
+            ) : (
               <Link to="/sign-up" prefetch="intent">
                 Sign Up
               </Link>
